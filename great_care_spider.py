66dac03ff1a8efa25f41ba6e2cbf25dfc7b7bfc5
Giorgi Kveniashvili <giorgi.kveniashvili@singular.uk>
2023-05-08 14:43:50 +0400

    Split name and surname

--- great_care_spider.py
+++ great_care_spider.py
@@ -14,24 +14,40 @@ class GreatCareSpider(CrawlSpider):
 
     rules = (
         # Extract and follow all links!
-        Rule(LinkExtractor(allow=('/doctors/')), callback='parse_item', follow=True),
+        Rule(LinkExtractor(allow=['/doctors/', '/optometrists/', '/nurses/', '/dentists/', 
+                                  '/physiotherapists/', '/dietitians/', '/occupationaltherapists/']), 
+                                  callback='parse_item', follow=True),
     )
 
     def parse_item(self, response):
         global file_index
 
         for title in response.xpath(title_xpath):
-            ExcelWorker.write_file(file_index, title.css('::text').get(), response.url)
+            full_name_formated = split_name(title.css('::text').get())
+            name = full_name_formated[0]
+            surname = full_name_formated[1]
+
+            write_file(file_index, name, surname, response.url)
             file_index += 1
         self.log('crawling'.format(response.url))
 
-class ExcelWorker():
-    def write_file(index, name, surname):
-        wb = openpyxl.load_workbook(file_path)
-        ws = wb.get_sheet_by_name('Sheet')
 
-        ws.cell(row=index, column=1).value = name
-        ws.cell(row=index, column=2).value = surname
+def write_file(index, name, surname, domain):
+    wb = openpyxl.load_workbook(file_path)
+    ws = wb.get_sheet_by_name('Sheet')
 
-        wb.save(file_path)
-        wb.close()
+    ws.cell(row=index, column=1).value = name
+    ws.cell(row=index, column=2).value = surname
+    ws.cell(row=index, column=3).value = domain
+
+    wb.save(file_path)
+    wb.close()
+
+def split_name(full_name):
+    if(len(full_name.split(' ')) > 2):
+        name = ' '.join(full_name.split(' ', 2)[:2])
+        surname = ' '.join(full_name.split(' ', 2)[2:])
+        return [name, surname]
+    name = full_name.split(' ', 1)[0]
+    surname = full_name.split(' ', 1)[1]
+    return [name, surname]
