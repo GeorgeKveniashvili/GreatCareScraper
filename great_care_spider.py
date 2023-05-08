c88dc3d838de1869dd3b7b7c8f6e9214d04bf7a9
Giorgi Kveniashvili <giorgi.kveniashvili@singular.uk>
2023-05-08 17:25:06 +0400

    added work, specification and profile

--- great_care_spider.py
+++ great_care_spider.py
@@ -3,7 +3,14 @@ from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 
 grat_care_url = 'https://www.iwantgreatcare.org/search'
+allowed_extractor = ['/doctors/', '/optometrists/', '/nurses/', '/dentists/', '/physiotherapists/', '/dietitians/', '/occupationaltherapists/']
 title_xpath = '//*[@id="entity-name-score-container"]/h1'
+specialises_xpath = '//*[@id="specialies-container"]/div/ul'
+#//*[@id="information"]/div[2]/div[2]/div/ul
+
+works_xpaths = ['//*[@id="works-at-container"]/div/ul', '/html/body/div[1]/main/div/span/div[2]/div/div[2]/div[1]/ul',
+                '//*[@id="information"]/div[2]/div[1]/div/ul', '//*[@id="information"]/div[1]/div[1]/div/ul']
+postcode_xpath = 'sss'
 file_path = "test101.xlsx"
 file_index = 2
 
@@ -14,9 +21,7 @@ class GreatCareSpider(CrawlSpider):
 
     rules = (
         # Extract and follow all links!
-        Rule(LinkExtractor(allow=['/doctors/', '/optometrists/', '/nurses/', '/dentists/', 
-                                  '/physiotherapists/', '/dietitians/', '/occupationaltherapists/']), 
-                                  callback='parse_item', follow=True),
+        Rule(LinkExtractor(allow=('/doctors/')), callback='parse_item', follow=True),
     )
 
     def parse_item(self, response):
@@ -27,23 +32,49 @@ class GreatCareSpider(CrawlSpider):
             name = full_name_formated[0]
             surname = full_name_formated[1]
 
-            write_file(file_index, name, surname, response.url)
+            specialises_selector = response.xpath(specialises_xpath)
+            specialises_list = specialises_selector.css('li::text').getall()
+            specialises = ""
+            for item in specialises_list:
+                specialises += str(item)
+            
+            profile = "Empty"
+
+            works = extract_works(response)
+
+            write_file(file_index, name, surname, specialises, profile, works, postcode_xpath, response.url)
             file_index += 1
         self.log('crawling'.format(response.url))
 
+def extract_works(response):
+    for works_xpath in works_xpaths:
+        works_selector = response.xpath(works_xpath)
+        if works_selector is None:
+            continue
+        works_list = works_selector.css('a::text').getall()
+    works = ""
+    for item in works_list:
+        works += str(item)
+    return works
 
-def write_file(index, name, surname, domain):
+
+def write_file(index, name, surname, specialises, profile, works, postcode, domain):
     wb = openpyxl.load_workbook(file_path)
     ws = wb.get_sheet_by_name('Sheet')
 
     ws.cell(row=index, column=1).value = name
     ws.cell(row=index, column=2).value = surname
-    ws.cell(row=index, column=3).value = domain
+    ws.cell(row=index, column=3).value = specialises
+    ws.cell(row=index, column=4).value = profile
+    ws.cell(row=index, column=5).value = works
+    ws.cell(row=index, column=6).value = postcode
+    ws.cell(row=index, column=7).value = domain
 
     wb.save(file_path)
     wb.close()
 
 def split_name(full_name):
+
     if(len(full_name.split(' ')) > 2):
         name = ' '.join(full_name.split(' ', 2)[:2])
         surname = ' '.join(full_name.split(' ', 2)[2:])
