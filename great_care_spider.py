a869e23ccc928a46445e4f26fa0eba97fa436d5d
GeorgeKveniashvili <giorgikveniashvili@gmail.com>
2023-05-09 11:57:35 +0400

    Rename global variables

--- great_care_spider.py
+++ great_care_spider.py
@@ -2,43 +2,43 @@ import openpyxl
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 
-great_care_url = 'https://www.iwantgreatcare.org/search'
-allowed_extractor = ['/doctors/', '/optometrists/', '/nurses/', '/dentists/', '/physiotherapists/', '/dietitians/', '/occupationaltherapists/']
-title_xpath = '//*[@id="entity-name-score-container"]/h1'
-specialises_xpathes = ['//*[@id="specialies-container"]/div/ul', '//*[@id="information"]/div[2]/div[2]/div/ul', 
+GREAT_CARE_URL = 'https://www.iwantgreatcare.org/search'
+ALLOWED_EXTRACTOR = ['/doctors/', '/optometrists/', '/nurses/', '/dentists/', '/physiotherapists/', '/dietitians/', '/occupationaltherapists/']
+TITLE_XPATH = '//*[@id="entity-name-score-container"]/h1'
+SPECIALISES_XPATHES = ['//*[@id="specialies-container"]/div/ul', '//*[@id="information"]/div[2]/div[2]/div/ul', 
                        '//*[@id="information"]/div[1]/div[2]/div/ul']
-works_xpathes = ['//*[@id="works-at-container"]/div/ul', '//*[@id="information"]/div[2]/div[1]/div/ul', 
+WORKS_XPATHES = ['//*[@id="works-at-container"]/div/ul', '//*[@id="information"]/div[2]/div[1]/div/ul', 
                  '//*[@id="information"]/div[1]/div[1]/div/ul', '/html/body/div[1]/main/div/span/div[2]/div/div[2]/div[1]/ul',
                  '/html/body/div[1]/main/div/span/div[2]/div/div[2]/div[2]/div[1]/div/ul']
-profile_xpathes = ['/html/body/div[1]/main/div/span/div[2]/div/div[2]/div[4]/div/div/p', 
+PROFILE_XPATHES = ['/html/body/div[1]/main/div/span/div[2]/div/div[2]/div[4]/div/div/p', 
                    '/html/body/div[1]/main/div/span/div[2]/div/div[2]/div[2]/div/div/p']
-file_path = "DoctorInfo.xlsx"
-file_index = 2
+FILE_PATH = "DoctorInfo.xlsx"
+FILE_INDEX = 2
 
 class GreatCareSpider(CrawlSpider):
     name = 'greatcarespider'
     allowed_domains = ['iwantgreatcare.org']
-    start_urls = [great_care_url]
+    start_urls = [GREAT_CARE_URL]
 
     rules = (
         # Extract and follow all links!
-        Rule(LinkExtractor(allow=allowed_extractor), callback='parse_item', follow=True),
+        Rule(LinkExtractor(allow=ALLOWED_EXTRACTOR), callback='parse_item', follow=True),
     )
 
     def parse_item(self, response):
-        global file_index
+        global FILE_INDEX
         
-        for title in response.xpath(title_xpath):
+        for title in response.xpath(TITLE_XPATH):
             full_name_formated = split_name(title.css('::text').get())
             name = full_name_formated[0]
             surname = full_name_formated[1]
 
-            specialises = extract_multiple_selectors(response, specialises_xpathes, 'li::text', surname)
-            profile = extract_multiple_selectors(response, profile_xpathes, '::text', surname)
-            works = extract_multiple_selectors(response, works_xpathes, 'a::text', surname)
+            specialises = extract_multiple_selectors(response, SPECIALISES_XPATHES, 'li::text', surname)
+            profile = extract_multiple_selectors(response, PROFILE_XPATHES, '::text', surname)
+            works = extract_multiple_selectors(response, WORKS_XPATHES, 'a::text', surname)
 
-            write_file(file_index, name, surname, specialises, profile, works, response.url)
-            file_index += 1
+            write_file(FILE_INDEX, name, surname, specialises, profile, works, response.url)
+            FILE_INDEX += 1
         self.log('crawling'.format(response.url))
 
 def extract_multiple_selectors(response, xpathes, text_selector, name):
@@ -56,7 +56,7 @@ def extract_multiple_selectors(response, xpathes, text_selector, name):
 
 
 def write_file(index, name, surname, specialises, profile, works, domain):
-    wb = openpyxl.load_workbook(file_path)
+    wb = openpyxl.load_workbook(FILE_PATH)
     ws = wb.get_sheet_by_name('Sheet')
 
     ws.cell(row=index, column=1).value = name
@@ -66,7 +66,7 @@ def write_file(index, name, surname, specialises, profile, works, domain):
     ws.cell(row=index, column=5).value = works
     ws.cell(row=index, column=6).value = domain
 
-    wb.save(file_path)
+    wb.save(FILE_PATH)
     wb.close()
 
 def split_name(full_name):
